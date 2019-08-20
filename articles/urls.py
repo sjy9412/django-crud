0ad20785d2870c8595064981a9192e1b378a200e
sjy9412 <sjy9412@gmail.com>
2019-08-20 11:22:16 +0900

    07 | articles - CRUD - edit

--- articles/urls.py
+++ articles/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('new/', views.new),
     path('create/',views.create),
     path('<int:article_pk>/',views.detail),
-    path('<int:article_pk>/delete/',views.delete)
+    path('<int:article_pk>/delete/',views.delete),
+    path('<int:article_pk>/edit/',views.edit),
+    path('<int:article_pk>/update/',views.update),
 ]
--- articles/views.py
+++ articles/views.py
@@ -32,3 +32,17 @@ def delete(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     article.delete()
     return redirect('/articles/')
+
+def edit(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    content = request.GET.get('content')
+    article.content = content
+    article.save()
+    return redirect(f'/articles/{article.pk}/')

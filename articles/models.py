5b70f296eba514177ac394b0c73ef7c36ad88f1c
sjy9412 <sjy9412@gmail.com>
2019-09-11 15:48:33 +0900

    comment

--- articles/models.py
+++ articles/models.py
@@ -24,3 +24,13 @@ class Article(models.Model):
 # makemigrations : migration 파일 생성
 #                : DB 설계도 작성
 # migrate : migration 파일 DB 반영
+
+class Comment(models.Model):
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    # on_delete
+    # 1. CASCADE : 글이 삭제되었을 때 모든 댓글을 삭제
+    # 2. PROTECT : 댓글이 존재하면 글 삭제 안됨
+    # 3. SET_NULL : 글이 삭제되면 NULL로 치환(NOT NULL일 경우 옵션 사용X)
+    # 4. SET_DEFAULT : 디폴트 값으로 치환

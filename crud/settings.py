56f80af76004863332652528525f729fee2fa1da
sjy9412 <sjy9412@gmail.com>
2019-09-23 11:40:43 +0900

    21 | Add static files

--- crud/settings.py
+++ crud/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages', # messages framework
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', # static file css, js, image
     'django_extensions',
     'bootstrap4',
 ]
@@ -120,6 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
+# static file들을 모두 모아서 해당 URL로 표현된다. (무리 폴더를 뜻하는 것이 아니다.)
+# /static/bootstrap
+# /static/articles/style.css
+STATIC_URL = '/static/' 
+# static file 물리 위치 지정
+# 기본적으로는 app에 있는 static 폴더들을 모두 관리하며, 아래에 임의의 폴더들을 추가할 수 있따.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'crud', 'assets')
+]
 # MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage

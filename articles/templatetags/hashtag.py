44660d109ca445f80d5d2b6aa7f6afaa43321465
sjy9412 <sjy9412@gmail.com>
2019-10-24 17:33:13 +0900

    crud

--- articles/templatetags/hashtag.py
+++ articles/templatetags/hashtag.py
@@ -1,12 +1,14 @@
-import hashlib
+import re
 from django import template
 
 register = template.Library()
 
 @register.filter
 def make_link(article):
-    content = article.content + ' '
+    # content = article.content + ' '
+    content = article.content
     hashtags = article.hashtags.all()
     for hashtag in hashtags:
-        content = content.replace(hashtag.content + ' ', f'<a href="/hashtags/{hashtag.pk}/">{hashtag.content}</a>')
+        # content = content.replace(hashtag.content + ' ', f'<a href="/hashtags/{hashtag.pk}/">{hashtag.content}</a>')
+        content = re.sub(f'{hashtag.content}\\b' , f'<a href="/hashtags/{hashtag.pk}/">{hashtag.content}</a>', content)
     return content
